4c0927158c20e6bc838ec9ed825cf8f4da9bfa49
Drake Wheeler <109574097+Copypaster3000@users.noreply.github.com>
2024-02-03 16:13:25 -0800

    Update front_trip.h

--- front_trip.h
+++ front_trip.h
@@ -1,9 +1,4 @@
 //front_trip.h
-//Drake Wheeler
-//Karla Fant
-//CS163
-//Program 4
-//5/21/2023
 //This file holds the struct front_trip which is used to temporarily hold details about a trip
 
 const int MAX {100};
